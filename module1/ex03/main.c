9afb917f1428e4016a6cad93c170e297a20ebc4e
rihoy <rihoy@f1br4s1.paris.42.school>
2026-04-16 14:14:59 +0200

    module1

---
+++ module1/ex03/main.c
@@ -0,0 +1,61 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rihoy <rihoy@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/04/15 20:14:51 by rihoy             #+#    #+#             */
+/*   Updated: 2026/04/16 14:03:55 by rihoy            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <util/delay.h>
+
+// 15.2.1 Registers
+// registre summary
+void	timer_init() {
+    DDRB |= (1 << DDB1);
+
+    TCCR1A |= (1 << WGM11);
+    TCCR1B |= (1 << WGM13) | (1 << WGM12);
+
+    TCCR1A |= (1 << COM1A1);
+
+    ICR1 = 15624; // definition de la periode
+
+    OCR1A = 1562; // rapport cyclique
+
+    TCCR1B |= (1 << CS12) | (1 << CS10);
+}
+
+void	ocr_modif_percent(uint16_t percent) {
+	OCR1A = ((uint32_t)ICR1 * percent) / 10;
+}
+
+int	main(void) {
+	uint16_t percent = 1;
+	DDRB |= (1 << DDB1);
+	DDRD &= ~((1 << PORTD2) | (1 << PORTD4));
+	PORTD |= (1 << PORTD2) | (1 << PORTD4);
+
+	timer_init();
+	while (1) {
+		_delay_ms(50);
+		if (!(PIND & (1 << PORTD2))) {
+			if (percent < 10)
+				percent++;
+			while (!(PIND & PORTD2)) {}
+		}
+		if (!(PIND & (1 << PORTD4))) {
+			if (percent > 1)
+				percent--;
+		while (!(PIND & PORTD4)) {}			
+		}
+		ocr_modif_percent(percent);
+		_delay_ms(50);
+	}
+	return (0);
+}
